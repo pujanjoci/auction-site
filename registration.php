5ae6404dd0671bcdd9574eab47de3634a95eed0f
pujanjoci <119594337+pujanjoci@users.noreply.github.com>
2023-09-12 11:13:33 +0545

    Update registration.php

--- registration.php
+++ registration.php
@@ -59,8 +59,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                 $mail->isSMTP();
                 $mail->Host = 'smtp.gmail.com';
                 $mail->SMTPAuth = true;
-                $mail->Username = 'rachitauction@gmail.com';
-                $mail->Password = 'yogmyaihjmimbcfb';
+                $mail->Username = 'Your@mail.com';
+                $mail->Password = 'Your_App_Password';
                 $mail->SMTPSecure = 'tls';
                 $mail->Port = 587;
 
